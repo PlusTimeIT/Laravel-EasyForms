3e3528e31041951e52f15ab4901c1a062af4c7c8
unknown <lu.tics@gmail.com>
2020-09-16 12:25:52 +1000

    0.0.2

--- src/Core/Base.php
+++ src/Core/Base.php
@@ -3,18 +3,19 @@
 namespace Second2None\EasyForms\Core;
 
 use Second2None\EasyForms\Core\DB\Connector;
+use Second2None\EasyForms\Core\Models\Settings;
 
 use DB;
 use Schema;
 
 class Base {
 
-        public function initiate_check(){
+        public static function initiate_check(){
             if ( ! Schema::hasTable( Connector::$settings_table ) || ! Schema::hasTable( Connector::$forms_table ) || ! Schema::hasTable( Connector::$form_fields_table ) || ! Schema::hasTable( Connector::$errors_table ) ) {
-                
-                
-            
                 throw new \Exception( 'Must run migration and seeder for database tables' );
             }
+            $setup_check = Settings::where( 'name' , 'initial_setup' )->first();
+                throw new \Exception( 'Error with seeding - run seeder' );
         }
+
 }
