b25f8e9b5777cb0032cea0f82771d7a0b06be4f1
Lion Eagle Solutions <lioneagle@MacBook-Pro.local>
2021-02-01 07:20:03 +1000

    Fix base directories
    Directories listed in Base::getDirectories() had a '\' which was causing file not found errors.
    
    Updates '\' to '/'.
    
    eg
    $package_directories[ 'config' ]      = $package_directories[ 'base' ] . '\config';
    updated to;
    $package_directories[ 'config' ]      = $package_directories[ 'base' ] . '/config';

--- src/Core/Base.php
+++ src/Core/Base.php
@@ -2,45 +2,47 @@
 
 namespace PlusTimeIT\EasyForms\App\Core;
 
-use PlusTimeIT\EasyForms\App\DB\Connector;
-
-use PlusTimeIT\EasyForms\App\Core\Settings;
-
 use DB;
+
 use File;
-use Schema;
+
 use Log;
+use PlusTimeIT\EasyForms\App\Core\Settings;
+use PlusTimeIT\EasyForms\App\DB\Connector;
+use Schema;
 
 // use PlusTimeIT\EasyForms\App\Core\Base;
-class Base {
+class Base
+{
+    public $directories;
 
-        public $directories;
+    public function __construct()
+    {
+        $this->directories = self::getDirectories();
+    }
 
-        public function __construct(){
-            $this->directories = self::getDirectories();
-        }
+    public static function isVendorPublished() : bool
+    {
+        return Settings::get('vendor_published') ?? false;
+    }
 
-        public static function isVendorPublished() : bool {
-            return Settings::get( 'vendor_published' ) ?? false;
-        }
+    public static function getDirectories() : object
+    {
+        $package_directories = [];
+        $package_directories[ 'base' ]        = dirname(dirname(dirname(__FILE__)));
 
-        public static function getDirectories() : object {
-            $package_directories = [];
-            $package_directories[ 'base' ]        = dirname( dirname( dirname( __FILE__ ) ) );
-            
-            // base directories
-            $package_directories[ 'config' ]      = $package_directories[ 'base' ] . '\config';
-            $package_directories[ 'resources' ]   = $package_directories[ 'base' ] . '\resources';
-            $package_directories[ 'routes' ]      = $package_directories[ 'base' ] . '\routes';
-            $package_directories[ 'src' ]         = $package_directories[ 'base' ] . '\src';
-            $package_directories[ 'views' ]       = $package_directories[ 'base' ] . '\views';
+        // base directories
+        $package_directories[ 'config' ]      = $package_directories[ 'base' ] . '/config';
+        $package_directories[ 'resources' ]   = $package_directories[ 'base' ] . '/resources';
+        $package_directories[ 'routes' ]      = $package_directories[ 'base' ] . '/routes';
+        $package_directories[ 'src' ]         = $package_directories[ 'base' ] . '/src';
+        $package_directories[ 'views' ]       = $package_directories[ 'base' ] . '/views';
 
-            // resources directories
-            $package_directories[ 'assets' ]      = $package_directories[ 'resources' ] . '\assets';
-            $package_directories[ 'components' ]  = $package_directories[ 'assets' ] . '\components';
-            $package_directories[ 'data' ]        = $package_directories[ 'resources' ] . '\data';
+        // resources directories
+        $package_directories[ 'assets' ]      = $package_directories[ 'resources' ] . '/assets';
+        $package_directories[ 'components' ]  = $package_directories[ 'assets' ] . '/components';
+        $package_directories[ 'data' ]        = $package_directories[ 'resources' ] . '/data';
 
-            return ( object ) $package_directories;
-        }
-
+        return ( object ) $package_directories;
+    }
 }
