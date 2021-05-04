e7ff27f1f1935e3abb44f3429543229a842c4df1
Kane Grills <kane@plustime.com.au>
2021-05-04 15:27:54 +1000

    php _cs updates I assume?

--- src/Controllers/Pages.php
+++ src/Controllers/Pages.php
@@ -3,25 +3,26 @@ namespace PlusTimeIT\EasyForms\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use PlusTimeIT\EasyForms\Controllers\Users;
 
 class Pages extends Controller
 {
+    protected $form_namespace = 'PlusTimeIT\\EasyForms\\Forms\\';
+
     public function example(request $request)
     {
         $example_id = $request->example_id ?? 1;
         $form_class = "{$this->form_namespace}ExampleForm{$example_id}";
 
-        if( $example_id < 4 ){
+        if ($example_id < 4) {
             return view('vendor.plustime-it.laravel-easyforms.examples.example-template')
-            ->with('load', $example_id == 1 ? 'page' : 'axios')
-            ->with('example', $example_id == 1 ? (new $form_class())->preFill() : (new $form_class()));
+                ->with('load', $example_id == 1 ? 'page' : 'axios')
+                ->with('example', $example_id == 1 ? (new $form_class())->preFill() : (new $form_class()));
         }
 
         $userList = Users::getAllUsers();
         return view('vendor.plustime-it.laravel-easyforms.examples.example-' . $example_id)
             ->with('load', 'axios')
-            ->with('userList',  $userList )
+            ->with('userList', $userList)
             ->with('example', new $form_class());
     }
 
@@ -29,6 +30,4 @@ class Pages extends Controller
     {
         return view('vendor.plustime-it.laravel-easyforms.examples.example-home');
     }
-
-    protected $form_namespace = 'PlusTimeIT\\EasyForms\\Forms\\';
 }
