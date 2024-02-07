44dcf483b2bca1cf6d2111c26e3f9ab82c1749dc
PlusTimeIT <kane@plustime.com.au>
2024-02-07 23:06:47 +1000

    update: 0.2.24

--- src/Services/GoogleRecaptcha.php
+++ src/Services/GoogleRecaptcha.php
@@ -8,7 +8,7 @@ use PlusTimeIT\EasyForms\Base\InputForm;
 
 class GoogleRecaptcha
 {
-    public static function verify(\Illuminate\Http\Request $request, InputForm | ActionForm $form): bool|array
+    public static function verify(\Illuminate\Http\Request $request, InputForm|ActionForm $form): bool|array
     {
         $token = $request->input('recaptcha_token');
         $secret = config('easyforms.form.google_recaptcha.secret_key');
@@ -20,24 +20,23 @@ class GoogleRecaptcha
         ]);
 
         $response = $response->json();
-        if (!$response['success']) {
+        if (! $response['success']) {
             return $response['error-codes'];
         }
 
         // check action is set correctly for action forms
         if ($form instanceof ActionForm) {
-            if ($response['action'] != 'process_form_' . str_replace('\\', '_', $request->input('form_name')) . '_' . str_replace('\\', '_', $request->input('form_action'))) {
+            if ($response['action'] != 'process_form_'.str_replace('\\', '_', $request->input('form_name')).'_'.str_replace('\\', '_', $request->input('form_action'))) {
                 return ['form' => 'Action does not meet requirements'];
             }
         }
 
         // check action is set correctly for input forms
         if ($form instanceof InputForm) {
-            if ($response['action'] != 'process_form_' . str_replace('\\', '_', $request->input('form_name'))) {
+            if ($response['action'] != 'process_form_'.str_replace('\\', '_', $request->input('form_name'))) {
                 return ['form' => 'Action does not meet requirements'];
             }
         }
-
 
         // check score is greater than minimum supplied
         if ($response['score'] < config('easyforms.form.google_recaptcha.minimum_score')) {
